5e78a26ac7c1bb52ba5a268733cc8c27dc8d356f
LiuXutong <liuxt@mail.ustc.edu.cn>
2015-08-14 19:10:10 +0800

    upload comments & delete some files

--- collinear/Brute.java
+++ collinear/Brute.java
@@ -8,56 +8,46 @@
 import java.util.Comparator;
 import java.util.Arrays;
 public class Brute{
-    private static class order implements Comparator<Point>{
-        public int compare(Point p, Point q){
-            return p.compareTo(q);
-        }
-    }
     public static void main(String[] args) {
-        Comparator<Point> order = new order();
-        StdDraw.setXscale(0, 32768);
+        StdDraw.setXscale(0, 32768);                            // basic settings for draw method
         StdDraw.setYscale(0, 32768);
         StdDraw.show(0);
         StdDraw.setPenRadius(0.01);
         
         String filename = args[0];
         In in = new In(filename);
-        int N = in.readInt();
-        Point[] p = new Point[N];
+        int N = in.readInt();                                   // number of the points
+        Point[] p = new Point[N];                               // point array
+        //draw all the points
         for(int i = 0; i < N; i++){
             int x = in.readInt();
             int y = in.readInt();
             p[i] = new Point(x, y);
             p[i].draw();
-
-           // StdOut.println(p[i]);
         }
+
         for(int i = 0; i < N; i++){
             for(int j = 0; j < N; j++){
                 for(int k = 0; k < N; k++){
                     for(int l = 0; l < N; l++){
-                        //if(p[i].compareTo(p[j]) < 0 && p[j].compareTo(p[k]) < 0
-                         //&& p[k].compareTo(p[l]) < 0){
-                            if(p[i].SLOPE_ORDER.compare(p[j], p[k]) == 0 && 
-                            p[i].SLOPE_ORDER.compare(p[j], p[l]) == 0 ){
-                        if(p[i].compareTo(p[j]) < 0 && p[j].compareTo(p[k]) < 0
-                         && p[k].compareTo(p[l]) < 0){
-
-                                 StdOut.println(p[i] + " -> " + p[j] + " -> " + p[k] + " -> " + p[l]);
-                                 Point[] temp = new Point[4];
-                                 temp[0] = p[i];
-                                 temp[1] = p[j];
-                                 temp[2] = p[k];
-                                 temp[3] = p[l];
-                                 Arrays.sort(temp, 0, 4, order);
-                                 temp[0].drawTo(temp[3]);
+                        if(p[i].SLOPE_ORDER.compare(p[j], p[k]) == 0 && 
+                                p[i].SLOPE_ORDER.compare(p[j], p[l]) == 0 ){ 
+                            if(p[i].compareTo(p[j]) < 0 && p[j].compareTo(p[k]) < 0
+                                    && p[k].compareTo(p[l]) < 0){
+                                StdOut.println(p[i] + " -> " + p[j] + " -> " + p[k] + " -> " + p[l]); // standard output
+                                Point[] temp = new Point[4];
+                                temp[0] = p[i];
+                                temp[1] = p[j];
+                                temp[2] = p[k];
+                                temp[3] = p[l];
+                                Arrays.sort(temp, 0, 4);        // sort the array in natural order
+                                temp[0].drawTo(temp[3]);        //draw from the beginning point and ending point as instructed
                                     }
-                            }
-                                //}
-                    }
+                                }
                 }
             }
         }
+    }
         StdDraw.show(0);
         StdDraw.setPenRadius();
     }
--- collinear/Fast.java
+++ collinear/Fast.java
@@ -7,21 +7,16 @@
 import java.util.Arrays;
 import java.util.Comparator;
 public class Fast {
-    /*private static class order implements Comparator<Point>{
-        public int compare(Point p, Point q){
-            return p.compareTo(q);
-        }
-    }*/
+
     public static void main(String[] args){
+        // basic settings for draw method 
         StdDraw.setXscale(0, 32768);
         StdDraw.setYscale(0, 32768);
         StdDraw.show(0);
         StdDraw.setPenRadius(0.01);
 
-
-      //  Comparator<Point> order = new order();
         String filename = args[0];
-        int count = 0;
+        int count = 0;                                  
         In in = new In(filename);
         int N = in.readInt();
         Point[] p = new Point[N];
@@ -32,47 +27,46 @@ public class Fast {
             p[i].draw();
         }
         Point[] temp = new Point[N];
-        Arrays.sort(p, 0, N); 
-        temp = Arrays.copyOf(p, N);
+        Arrays.sort(p, 0, N);                               // sort the array in the natural order 
+        temp = Arrays.copyOf(p, N);                         // copy point array p to the array auxiliary temp
         for(int i = 0; i < N; i++){
             temp[i] = p[i];
-            //StdOut.println("5: "+p[i]);
+            //StdOut.println("5: "+p[i]);                   // for test
         }
         for(int i = 0; i < N; i++){
             Point q = temp[i];
-            //StdOut.println("1: " + q);
+            //StdOut.println("1: " + q);                    // for test
             Arrays.sort(p, 0, N);
             Arrays.sort(p, 0, N, q.SLOPE_ORDER);
             /*for(int j = 0; j < N ; j++){
-                StdOut.println("2: " + p[j] + " " + q.slopeTo(p[j]));
+                StdOut.println("2: " + p[j] + " " + q.slopeTo(p[j])); // for test
             }*/
             for(int j = 0; j < N - 2; j+=(count+1)){
-                //if(q.compareTo(p[j]) < 0 && q.compareTo(p[j+1]) < 0 && q.compareTo(p[j+2]) < 0){
-                 //       StdOut.println("3: " + q + " -> " + p[j] + " -> " + p[j+1] + " -> " + p[j+2]);
-                count = 0;
+                 //       StdOut.println("3: " + q + " -> " + p[j] + " -> " + p[j+1] + " -> " + p[j+2]); // for test
+                count = 0;                                  // number of the eaual points is count + 2
                 for(int k = j; k < N - 1; k++){
-                    if(q.SLOPE_ORDER.compare(p[k], p[k+1]) == 0){
-                        //if (q.compareTo(new Point(8000, 8000)) == 0) StdOut.println("count: " + count +" p[j] " + p[j] +" k: " + p[k] + p[k+1] + " slope " + q.slopeTo(p[k]) + " " + q.slopeTo(p[k+1]));
+                    if(q.SLOPE_ORDER.compare(p[k], p[k+1]) == 0){ // find equal pares
+                        //if (q.compareTo(new Point(8000, 8000)) == 0) StdOut.println("count: " + count +" p[j] " + p[j] +" k: " + p[k] + p[k+1] + " slope " + q.slopeTo(p[k]) + " " + q.slopeTo(p[k+1]));                                                       // for test
                         count++;
-                        if(k == N - 2) {
-                        if(count >= 2){
-                            if(q.compareTo(p[j]) <= 0){
-                                for(int l = 0; l < 2 + count;l++) {
-                                    if(l > 0 && l < 1 + count){
-                                        StdOut.print(p[j + l - 1] + " -> ");
+                        if(k == N - 2) {                    // coner case
+                            if(count >= 2){
+                                if(q.compareTo(p[j]) <= 0){
+                                    for(int l = 0; l < 2 + count;l++) { // standrad output
+                                        if(l > 0 && l < 1 + count){
+                                            StdOut.print(p[j + l - 1] + " -> ");
+                                        }
+                                        else if(l == count + 1){
+                                            StdOut.print(p[j + l - 1] + "\n");
+                                        }
+                                        else    StdOut.print(q + " -> ");
                                     }
-                                    else if(l == count + 1){
-                                        StdOut.print(p[j + l - 1] + "\n");
-                                    }
-                                    else    StdOut.print(q + " -> ");
-                                }
-                                q.drawTo(p[j + count]);
-                            } 
-                        }
+                                    q.drawTo(p[j + count]);
+                                } 
+                            }
                         }
                     }
-                    else{
-                        if(count >= 2){
+                    else{                                   // if curPoint not equal with next point
+                        if(count >= 2){                     // 4 or more points
                             if(q.compareTo(p[j]) <= 0){
                                 for(int l = 0; l < 2 + count;l++) {
                                     if(l > 0 && l < 1 + count){
@@ -84,38 +78,17 @@ public class Fast {
                                     else    StdOut.print(q + " -> ");
                                 }
                                 q.drawTo(p[j + count]);
-                            /*r[0] = q;
-                            for(int l = 1; l < 2 + count;l++){
-                                r[l] = p[j + l - 1];
-                            //    StdOut.println("4: "+ r[l]);
-                            }
-                            Arrays.sort(r, 0, r.length);
-                                //StdOut.println("3: " + "i: " + i + "j: " + j + "k: " + k);
-                                for(int l = 0; l < r.length; l++){
-                                    if(l == 0){
-                                        StdOut.print(r[l] + " -> ");
-                                    }
-                                    else if(l == r.length - 1){
-                                        StdOut.print(r[l] + "\n");
-                                        r[0].drawTo(r[l]);
-                                    }
-                                    else StdOut.print(r[l] + " -> ");
-                                }
-                               */ 
-                                //StdOut.println("break: " + p[j] + " count: " + count);
+                                //StdOut.println("break: " + p[j] + " count: " + count);    // for test
                                 break;
                             }
                             break;
                         }
                         else{
-                            //StdOut.println("break2: " + p[j] + " count: " + count);                           
+                            //StdOut.println("break2: " + p[j] + " count: " + count);       // for test
                             break;
                         }
                     }
                 }
-                //}
-
-                 
             }
         }
         StdDraw.show(0);
